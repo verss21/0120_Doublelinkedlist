0a141ece63c12b4a7a0e6ed60f3d15267c1845e7
verss21 <arjunafrizal01@gmail.com>
2024-05-04 22:42:10 +0700

    else

--- doublelinked/doublelinked.cpp
+++ doublelinked/doublelinked.cpp
@@ -36,4 +36,10 @@ void addNode()
 		newNode->prev = NULL; // step 5 : make the new node point to NULL
 		START = newNode; // step 6 : make the new node the first node
 	}
-	
+	else
+	{
+		// insert the new node in the middle or at the end
+		Node* current = START; // step 1.a : start from the first node
+		Node* previous = NULL; // step 1.b : previous node is NULL initially
+
+		
