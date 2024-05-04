21a775d83800ae6f07e083418724ab6783e2e39e
verss21 <arjunafrizal01@gmail.com>
2024-05-04 22:37:56 +0700

    step 2

--- doublelinked/doublelinked.cpp
+++ doublelinked/doublelinked.cpp
@@ -19,4 +19,12 @@ void addNode()
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name; // Assign value to the data field of the new node
 
-	
+	//Insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{ // step 2 : insert the new node at the beginning
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+		
