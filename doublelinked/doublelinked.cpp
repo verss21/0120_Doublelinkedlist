d97710e396880117e9d3a5f3a0a67b1f6cd70941
verss21 <arjunafrizal01@gmail.com>
2024-05-04 22:37:09 +0700

    step 1 create new node

--- doublelinked/doublelinked.cpp
+++ doublelinked/doublelinked.cpp
@@ -12,3 +12,11 @@ struct Node
 Node* START = NULL;
 
 void addNode()
+{
+	Node* newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; // Assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // Assign value to the data field of the new node
+
+	
