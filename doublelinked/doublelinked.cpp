2858b39811afd2e8f6f55e1a150986b7106ade6b
verss21 <arjunafrizal01@gmail.com>
2024-05-04 22:41:33 +0700

    step 5 dan 6

--- doublelinked/doublelinked.cpp
+++ doublelinked/doublelinked.cpp
@@ -32,4 +32,8 @@ void addNode()
 		if (START != NULL)
 		{
 			START->prev = newNode; // step 4 : make the first node point to the new node
-		
+		}
+		newNode->prev = NULL; // step 5 : make the new node point to NULL
+		START = newNode; // step 6 : make the new node the first node
+	}
+	
