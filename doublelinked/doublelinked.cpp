a1ccd109dbd5e1ed9a698168f045bb2f3bf05d68
verss21 <arjunafrizal01@gmail.com>
2024-05-04 22:43:44 +0700

    bool search

--- doublelinked/doublelinked.cpp
+++ doublelinked/doublelinked.cpp
@@ -59,4 +59,23 @@ void addNode()
 		{
 			previous->next = newNode; // step 7 Make the next field of the previous node point to the new node
 		}
-		
+		else
+		{
+			// If previous is still NULL, it means newNode is now the first node
+			START = newNode;
+		}
+	}
+}
+
+bool search(int rollNo, Node** previous, Node** current)
+{
+	*previous = NULL;
+	*current = START;
+	while (*current != NULL && (*current)->noMhs != rollNo)
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
+}
+
